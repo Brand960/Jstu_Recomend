b7f598bb579bb0ebeee3e338d5bc61d423056aa0
Yue <Yue>
2017-03-26 21:29:54 +0800

    api fix

--- main/views.py
+++ main/views.py
@@ -42,20 +42,27 @@ def get_by_page(_type,_page,_size):
 	res=json.loads(req.text)
 	return res
 
-def recommend_by_id(_type,_id):
-	if (_type=="book"):
-		url=URL+"books/recommend?id="+_id
-	elif(_type=="paper"):
-		url=URL+"papers/recommend?id="+_id
-	
+# def recommend_by_id(_type,_id):
+# 	if (_type=="book"):
+# 		url=URL+"books/recommend?id="+_id
+# 	elif(_type=="paper"):
+# 		url=URL+"papers/recommend?id="+_id
+# 	
+# 	req=requests.get(url)
+# 	res=json.loads(req.text)
+# 	return res
+
+def recommend__book_by_id(_type,_id):
+	url=URL+"/recommend/books?id="+_id+"&type="+_type
 	req=requests.get(url)
 	res=json.loads(req.text)
 	return res
 
-# def home(request):
-# 	res=http_get("1")
-# 	return render(request,'home.html',{'res':res,})
-
+def recommend__paper_by_id(_type,_id):
+	url=URL+"/recommend/papers?id="+_id+"&type="+_type
+	req=requests.get(url)
+	res=json.loads(req.text)
+	return res
 
 def home(request):
 	_page= str(random.randint(1, 2000))
@@ -86,17 +93,29 @@ def recommend(request):
 		res_paper_r=get_by_page("paper",_page,_size)
 		
 		_id=request.GET['id']
+		
+		_type=request.GET['type']
+		res_rec_paper=recommend__paper_by_id(_type, _id)
+		res_rec_book=recommend__book_by_id(_type, _id)
 
-		if request.GET['type']=="paper":
+		if _type=="paper":
 			res_paper=get_by_id("paper",_id)
-			res_rec_paper=recommend_by_id("paper",_id)
 			return render(request,'recommend.html',{'res_paper':res_paper,"res_paper_r":res_paper_r,"res_book_r":res_book_r,
-																		'res_rec_paper':res_rec_paper,})
-		if request.GET['type']=="book":
+ 																		'res_rec_paper':res_rec_paper,"res_rec_book":res_rec_book,})
+		elif _type=="book":
 			res_book=get_by_id("book",_id)
-			res_rec_book=recommend_by_id("book",_id)
 			return render(request,'recommend.html',{'res_book':res_book,"res_paper_r":res_paper_r,"res_book_r":res_book_r,
-																		'res_rec_book':res_rec_book,})
+ 																		'res_rec_paper':res_rec_paper,"res_rec_book":res_rec_book,})
+# 		if request.GET['type']=="paper":
+# 			res_paper=get_by_id("paper",_id)
+# 			res_rec_paper=recommend_by_id("paper",_id)
+# 			return render(request,'recommend.html',{'res_paper':res_paper,"res_paper_r":res_paper_r,"res_book_r":res_book_r,
+# 																		'res_rec_paper':res_rec_paper,})
+# 		if request.GET['type']=="book":
+# 			res_book=get_by_id("book",_id)
+# 			res_rec_book=recommend_by_id("book",_id)
+# 			return render(request,'recommend.html',{'res_book':res_book,"res_paper_r":res_paper_r,"res_book_r":res_book_r,
+# 																		'res_rec_book':res_rec_book,})
 		
 		
 	return HttpResponse("抱歉！未知错误，请刷新试试")
